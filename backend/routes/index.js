9fce22935ff717d00339b32859235330d34e34b2
Mark <markmart17@icloud.com>
2023-07-05 19:02:28 -0400

    add an api router

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -2,6 +2,9 @@ const express = require('express');
 
 const router = express.Router();
 
+const apiRouter= require('./api');
+router.use('/api', apiRouter);
+
 // router.get('/hello/world', function(req,res){
 //   res.cookie('XSRF-TOKEN', req.csrfToken());
 
@@ -15,5 +18,6 @@ router.get('/api/csrf/restore', (req,res) =>{
     'XSRF-Token': csrfToken
   });
 })
+
 
 module.exports = router;
