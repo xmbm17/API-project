1e66aae4bdca65c0d4be577d10c89f64d290e4d0
Mark <markmart17@icloud.com>
2023-07-07 10:24:26 -0400

    test

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -5,7 +5,6 @@ const bcrypt = require('bcryptjs');
 const { setTokenCookie, restoreUser } = require('../../utils/auth');
 const { User } = require('../../db/models');
 
-
 const router = express.Router();
 
 router.post(
